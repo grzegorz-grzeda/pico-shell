e1a75636c4c512e08a8d3600433079f2890f1afd
ggrzeda <grzegorz.grzeda@schange.com>
2021-08-04 19:34:42 +0200

    Changes in naming conventions

--- src/psh.c
+++ src/psh.c
@@ -1,6 +1,33 @@
 #include "psh.h"
 
-int psh_init(psh_cli *cli, psh_node* root_node, psh_node* bin_node){}
-int psh_mount_directory(psh_node* parent, psh_node* directory){}
-int psh_mound_file(psh_node* parent, psh_node* file){}
-int psh_process(psh_cli* cli){}
+int psh_init(psh_cli *cli, psh_directory *root_node, psh_directory *bin_node)
+{
+}
+
+int psh_mount_directory(psh_directory *parent, const char *name)
+{
+}
+
+int psh_mound_file(psh_directory *parent, psh_file *file)
+{
+}
+
+psh_file psh_open(const char *name)
+{
+}
+
+int psh_write(psh_file fd, char *buffer, int count)
+{
+}
+
+int psh_read(psh_file fd, char *buffer, int count)
+{
+}
+
+int psh_close(psh_file fd)
+{
+}
+
+int psh_process(psh_cli *cli)
+{
+}
--- src/psh.h
+++ src/psh.h
@@ -5,31 +5,41 @@
  * @{
  */
 
-
-typedef enum psh_node_type_t{
+typedef enum psh_node_type_t
+{
     PSH_NODE_FILE = 0,
     PSH_NODE_DIR
-}psh_node_type;
+} psh_node_type;
 
 struct psh_node_t;
-typedef int (*psh_file_io_cb)(struct psh_node_t* file, char *buffer, int count);
-typedef int (*psh_file_exe_cb)(struct psh_node_t* file, int argc, char **argv);
+typedef int (*psh_file_io_cb)(struct psh_node_t *file, char *buffer, int count);
+typedef int (*psh_file_exe_cb)(struct psh_node_t *file, int argc, char **argv);
 
-typedef struct psh_node_t{
-    const char* name;
-    const char* help;
+struct psh_node_base_t;
+typedef struct psh_node_base_t
+{
+    const char *name;
+    psh_node_type _type;
+    struct psh_node_base_t *_parent;
+    struct psh_node_base_t *_next;
+} psh_node_base;
+
+typedef struct psh_file_node_t
+{
+    psh_node_base _base;
+    const char *help;
     psh_file_io_cb read;
     psh_file_io_cb write;
     psh_file_exe_cb execute;
-    psh_node_type _type;
-    struct psh_node_t *_parent;
-    struct psh_node_t *_next;
-    struct psh_node_t *_children;
     struct psh_node_t *_file_descriptor_list_next;
     int _is_open;
-}psh_node;
+} psh_file;
 
-typedef psh_node* psh_file;
+typedef struct psh_directory_node_t
+{
+    psh_node_base _base;
+    psh_node_base *_children;
+} psh_directory;
 
 /**
  * Callback definition for pico-shell printing and reading function
@@ -37,32 +47,35 @@ typedef psh_node* psh_file;
  * @return 0 if operation failed
  * @return 1 if operation succeded
  */
-typedef int (*psh_cli_io_cb)(char*);
+typedef int (*psh_cli_io_cb)(char *);
 
-typedef struct psh_cli_t{
-    struct {
+typedef struct psh_cli_t
+{
+    struct
+    {
         char *buffer;
         int size;
         psh_cli_io_cb io;
     } input;
-    struct {
+    struct
+    {
         char *buffer;
         int size;
         psh_cli_io_cb io;
     } output;
-    psh_node *root;
-}psh_cli;
+    psh_directory *root;
+} psh_cli;
 
-int psh_init(psh_cli *cli, psh_node* root_node, psh_node* bin_node);
-int psh_mount_directory(psh_node* parent, psh_node* directory);
-int psh_mound_file(psh_node* parent, psh_node* file);
+int psh_init(psh_cli *cli, psh_directory *root_node, psh_directory *bin_node);
+int psh_mount_directory(psh_directory *parent, const char *name);
+int psh_mound_file(psh_directory *parent, psh_file *file);
 
 psh_file psh_open(const char *name);
 int psh_write(psh_file fd, char *buffer, int count);
 int psh_read(psh_file fd, char *buffer, int count);
 int psh_close(psh_file fd);
 
-int psh_process(psh_cli* cli);
+int psh_process(psh_cli *cli);
 
 /**
  * @}
